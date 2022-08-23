37d82c98479d6ca162ef1e6755b4baf679e2fc0b
Badger <jonBraunsma@gmail.com>
2022-08-23 19:50:20 -0400

    Environment Setup (#8)
    * Changed environment setup process
    
    * - Added Health check
    - Added auto-migrations to start-db script
    
    * Adjusted scripts so they can be ran from anywhere
    
    - Can now run the scripts via right click -> run

---
+++ create-env.ps1
@@ -0,0 +1,58 @@
+Param(
+    [Parameter(Mandatory=$false)] [string] $databasePath,
+    [Parameter(Mandatory=$false)] [string] $dbPassword,
+    [Parameter(Mandatory=$false)] [string] $dbName,
+    [Parameter(Mandatory=$false)] [string] $dbHost
+)
+
+$basePath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$envPath = Join-Path $basePath ".env"
+
+if(Test-Path $envPath)
+{
+    Remove-Item $envPath
+}
+
+# if the database path has not been set
+if(-not($databasePath))
+{
+    $databasePath = $basePath
+    Write-Output "Defaulting Database Path to $databasePath"
+}
+
+if(-not($dbPassword))
+{
+    $dbPassword = "my-awesome-password"
+    Write-Output "Using Default Password"
+}
+
+if(-not($dbName))
+{
+    $dbName = "social-coder"
+    Write-Output "Using Default DB Name"
+}
+
+if(-not($dbHost)) 
+{ 
+    $dbHost = "localhost"
+    Write-Output "Using Default Host"
+}
+
+
+$settingsPath = [IO.Path]::Combine($basePath, "SocialCoder.Web", "Server", "appsettings.json")
+Write-Output "appsettings.json path: $settingsPath"
+$settings = Get-Content $settingsPath
+
+$connectionString = "server=$dbHost;user=root;password=$dbPassword;database=$dbName"
+$settings = $settings -replace ('"DefaultConnection": "(.*?)"', """DefaultConnection"": ""$connectionString""")
+
+write-output $settings
+
+Write-Output "Updating connection string..."
+Set-Content $settingsPath $settings
+
+New-Item $envPath
+Add-Content $envPath "DB_PATH=$databasePath\Database"
+Add-Content $envPath "DB_PASSWORD=$dbPassword"
+Add-Content $envPath "DB_HOST=$dbHost"
+Add-Content $envPath "DB_NAME=$dbName"
--- start-db.ps1
+++ start-db.ps1
@@ -1,1 +1,34 @@
-docker-compose up social-coder-api-db -d
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+cd $scriptPath
+
+$envPath = Join-Path $scriptPath .env
+
+docker-compose --env-file $envPath up -d social-coder-api-db
+
+$check = $false
+[int]$attempts = 10
+
+while($attempts -gt 0 -and (-not $check))
+{
+    Write-Output "Waiting for DB Container to come online..."
+    [string]$state = $(docker container inspect social-coder-api-db)
+    $check = $state -match 'healthy'
+
+    if($check)
+    {
+        break
+    }
+    
+    $attempts = $attempts -= 1
+    Write-Output "Waiting 5 seconds..."
+    Start-Sleep 5
+}
+
+$serverPath = [IO.Path]::Combine($scriptPath, "SocialCoder.Web", "Server")
+cd $serverPath
+
+Write-Output "Running migrations..."
+dotnet ef database update
+
+Write-Output "Complete..."
+cd $PSScriptRoot

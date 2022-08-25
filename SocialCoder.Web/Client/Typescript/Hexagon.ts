dc05f93522a027b52eace19e43fbb9f565e300bb
Badger <jonBraunsma@gmail.com>
2022-08-25 00:35:58 -0400

    - Added TypeScript support to project - Added Ignores for nullability checks in models - Added Math/Color Utilities - Added models which shall support member-card - Added plugin which will render hexagons

---
+++ SocialCoder.Web/Client/Typescript/Hexagon.ts
@@ -0,0 +1,70 @@
+﻿interface HexagonOptions 
+{
+    /**
+     * Classname to look for in the DOM
+     */
+    container: string;
+    
+    /**
+     * Width of hexagon
+     */
+    width: number;
+
+    /**
+     * Height of hexagon
+     */
+    height: number;
+
+    /**
+     * Should there be rounded corners?
+     */
+    roundedCorners: boolean;
+
+    /**
+     * Radius for rounded corners
+     */
+    roundedCornerRadius: number;
+
+    /**
+     * Filled?
+     */
+    fill: boolean;
+
+    /**
+     * Line color for hexagon
+     */
+    lineColor: string;
+
+    /**
+     * Line width for hexagon
+     */
+    lineWidth: number;
+
+    clip: boolean;
+    
+    gradient: GradientOptions;
+    scale: ScaleOptions;
+}
+
+interface GradientOptions
+{
+    colors: string[];
+}
+
+interface ScaleOptions 
+{
+    start: number;
+    end: number;
+    stop: number;
+}
+
+/**
+ * This function is tied to the xm_plugins.min.js file in the wwwroot/js folder
+ * @param options
+ */
+function createHexagon(options: HexagonOptions)
+{
+    // Badger: This is excluded because it's included as an external library
+    // @ts-ignore
+    return new XM_Hexagon(options);
+}
---
+++ SocialCoder.Web/Shared/ViewModels/UserSocialMediaItem.cs
@@ -0,0 +1,9 @@
+﻿using SocialCoder.Web.Shared.Enums;
+
+namespace SocialCoder.Web.Shared.ViewModels;
+
+public class UserSocialMediaItem
+{
+    public string Url { get; set; }
+    public SocialMediaType Type { get; set; }
+}

69700bdc7a4eefb24902828faca0cd6bc7333a60
Badger <jonBraunsma@gmail.com>
2022-08-25 19:24:48 -0400

    - Added avatar component - Added member card component

---
+++ SocialCoder.Web/Shared/ViewModels/BasicBadgeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialCoder.Web.Shared.ViewModels;
+
+public class BasicBadgeViewModel
+{
+    public int BadgeId { get; set; }
+    public string Title { get; set; }
+    public string ImagePath { get; set; }
+}

ffb22d7e921a05d147d4e86792a0ce7d0410fa8d
Badger <jonBraunsma@gmail.com>
2022-08-30 20:01:50 -0400

    Component for commenting/posting implemented

---
+++ SocialCoder.Web/Client/Components/AuthenticatedComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using SocialCoder.Web.Client.Services.Implementations;
+
+namespace SocialCoder.Web.Client.Components;
+
+public class AuthenticatedComponent : ComponentBase
+{
+    [Inject] protected IdentityAuthenticationStateProvider AuthProvider { get; set; }
+
+    protected ClaimsPrincipal User { get; private set; }
+    protected string Name { get; private set; }
+    
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        var auth = await AuthProvider.GetAuthenticationStateAsync();
+
+        User = auth.User ?? new();
+        Name = auth.User?.Identity?.Name ?? string.Empty;
+    }
+}

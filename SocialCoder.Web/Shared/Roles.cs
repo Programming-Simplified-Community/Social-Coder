eebfc4d13ddccb667baf5acbbd6bcffaec41c577
Badger <jonBraunsma@gmail.com>
2022-09-07 00:43:20 -0400

    Added role seed. First user with no admins will auto get admin.

---
+++ SocialCoder.Web/Shared/Roles.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialCoder.Web.Shared;
+
+public static class Roles
+{
+    /// <summary>
+    /// Highest level access
+    /// </summary>
+    public const string Administrator = "Administrator";
+    
+    /// <summary>
+    /// Access to event-based items
+    /// </summary>
+    public const string EventCoordinator = "Event-Coordinator";
+}

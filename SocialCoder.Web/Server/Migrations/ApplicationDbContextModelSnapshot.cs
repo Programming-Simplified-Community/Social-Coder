d0fd047a4f56e949bae6c1b9d62578bb02496ed2
Badger <jonBraunsma@gmail.com>
2022-09-14 01:04:18 -0400

    idk why this wasn't selected

--- SocialCoder.Web/Server/Migrations/ApplicationDbContextModelSnapshot.cs
+++ SocialCoder.Web/Server/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace SocialCoder.Web.Server.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.7")
+                .HasAnnotation("ProductVersion", "6.0.8")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
@@ -510,6 +510,40 @@ namespace SocialCoder.Web.Server.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("CodeJamTopics");
+                });
+
+            modelBuilder.Entity("SocialCoder.Web.Shared.Models.Management.PlatformUserBan", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("integer");
+
+                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("CreatedByUserId")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime>("CreatedOn")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.Property<string>("EditedByUserId")
+                        .HasColumnType("text");
+
+                    b.Property<DateTime?>("EditedOn")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.Property<string>("Reason")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<string>("UserId")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("PlatformBans");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
